c98fc85c47971874483f5bc2314806868c877210
nejdetkadir <nejdetkadir.550@gmail.com>
2021-02-13 17:15:45 +0300

    Prepare endpoints of CRUD operations

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,49 @@
 const express = require('express')
 const router = express.Router()
+const Article = require('../models/Article');
 
-/* GET test page. */
-router.get('/', (req, res) => {
-  res.status(200).json({
-    test: true
+/* GET articles */
+router.get('/articles', (req, res) => {
+  Article.find({}).then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    res.status(404).json(data)
+  })
+})
+
+/* POST create new article */
+router.post('/articles/new', (req, res) => {
+  const {title, text} = req.body
+  new Article({
+    title,
+    text
+  }).save()
+  .then(data => {
+    res.status(201).json(data)
+  }).catch(err => {
+    res.status(404).json(err)
+  })
+})
+
+/* PUT update article with by id */
+router.put('/articles/update', (req, res) => {
+  const {id, title, text} = req.body
+  Article.findByIdAndUpdate(id, {title, text}, {new: true})
+  .then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    res.status(404).json(err)
+  })
+})
+
+/* DELETE delete article with by id */
+router.delete('/articles/delete', (req, res) => {
+  const {id} = req.body
+  Article.findByIdAndDelete(id)
+  .then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    res.status(404).json(err)
   })
 })
 
