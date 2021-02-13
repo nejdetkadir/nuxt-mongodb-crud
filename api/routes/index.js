41f4ab758e99da3ab63dbe0db1faed7e035008a0
nejdetkadir <nejdetkadir.550@gmail.com>
2021-02-13 19:00:33 +0300

    Create endpoint of show article with by id

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,6 +11,16 @@ router.get('/articles', (req, res) => {
   })
 })
 
+/* GET article with by id */
+router.get('/articles/:id', (req, res) => {
+  Article.findById(req.params.id)
+  .then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    res.status(404).json(err)
+  })
+})
+
 /* POST create new article */
 router.post('/articles/new', (req, res) => {
   const {title, text} = req.body
