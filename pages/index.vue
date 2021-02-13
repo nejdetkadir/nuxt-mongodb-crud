a2a2b44d9fddbaf7006c6051496b12924b0a5103
nejdetkadir <nejdetkadir.550@gmail.com>
2021-02-13 20:57:15 +0300

    Add alert to ui for errors

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template lang="pug">
   .container.mt-5.mb-5
     .row
+      .col-md-12(v-if="this.$route.query.status")
+        .col-md-6.mx-auto
+          .alert.alert-danger.text-center {{messages[this.$route.query.status]}}
       .col-sm-6.col-md-4.col-lg-3.p-3(v-for="(article, index) in articles" :key="index")
         Article(:article="article")
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      messages: {
+        'notfound': 'Can not found an article',
+        'deleteerror': 'An error occurred while deleting article'
+      }
+    }
+  },
   asyncData({$axios}) {
     return $axios.get(`/api/articles`)
     .then(res => {
